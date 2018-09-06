20f89ecf2fcaa6c1f77078f794e7b03bd58c67dd
Tabata Mackenzie <tabata.mackenzie.univazo@everis.com>
2018-09-06 11:05:44 -0300

    se agregan dependencias de bdd y setup de appium

---
+++ cl.falabella.test.linkedin/src/main/java/cl/falabella/test/linkedin/Main.java
@@ -0,0 +1,29 @@
+package cl.falabella.test.linkedin;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class Main {
+	public static void main(String[] args) throws InterruptedException, MalformedURLException {
+		AppiumDriver<MobileElement> driver;
+		System.out.println("*****************************************************");
+		System.out.println("Setting up device and desired capabilities");
+		//com.linkedin.android
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "demo");
+		capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+		URL url = new URL("http://127.0.0.1:4723/wd/hub");
+		driver = new AndroidDriver<MobileElement>(url, capabilities);
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	}
+
+}
--- cl.falabella.test.linkedin/src/test/java/test/linkedin/login/LoginLinkedin.java
+++ cl.falabella.test.linkedin/src/test/java/test/linkedin/login/LoginLinkedin.java
@@ -1,17 +1,40 @@
 package test.linkedin.login;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 
-import io.appium.java_client.MobileElement;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class LoginLinkedin {
-	 public WebDriver driver;
+	@Given("^Navegar en la url \"([^\"]*)\"$")
+	public void navegar_en_la_url(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
 
-	    public void GuineaPigPage(WebDriver driver) {
-	        this.driver = driver;
-	        PageFactory.initElements(driver, this);
-	    }
+	@When("^Ingresar email \"([^\"]*)\"$")
+	public void ingresar_email(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^Ingresar contrase?a \"([^\"]*)\"$")
+	public void ingresar_contrase_a(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^se debe cargar perfil de linkedin$")
+	public void se_debe_cargar_perfil_de_linkedin() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Debe mostrarse un mensaje \"([^\"]*)\"$")
+	public void debe_mostrarse_un_mensaje(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
 }
